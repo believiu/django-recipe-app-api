4de8a437ba8149c7336070b91eefc20a9d31dc16
liviu.boscu <liviu.boscu@SVROLP01967.ro.softvision.global>
2020-12-08 17:41:16 +0200

    added create tag

--- app/recipe/tests/test_tags.py
+++ app/recipe/tests/test_tags.py
@@ -62,3 +62,29 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tag.name)
+
+    def test_create_tag_successful(self, tag_name="New"):
+        """Test creating a new tag"""
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=tag_name
+        ).exists()
+
+        self.assertFalse(exists)
+
+        payload = {"name": tag_name}
+        res = self.client.post(TAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=tag_name
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_failure(self, tag_name=""):
+        """Test creating a tag with invalid name fails"""
+        payload = {"name": tag_name}
+        res = self.client.post(TAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
